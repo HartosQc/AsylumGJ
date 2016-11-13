2fb8068762a2262ada1667216a79c969647454ee
Julien thibault <julienthibault244@gmail.com>
2016-11-13 09:23:47 -0500

    intencity play

--- AsylumGame/Assets/Script/CreepyBadTrip.cs
+++ AsylumGame/Assets/Script/CreepyBadTrip.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.ImageEffects;
 
 public class CreepyBadTrip : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private GameObject cam;
+    private VignetteAndChromaticAberration vignette;
+    private Vortex vortex;
+
+    // Use this for initialization
+    void Start () {
+        GameObject[] reds;
+        reds = GameObject.FindGameObjectsWithTag("MaineCamera");
+        cam = reds[0];
+
+        vignette = (VignetteAndChromaticAberration)cam.GetComponent(typeof(VignetteAndChromaticAberration));
+        vortex = (Vortex)cam.GetComponent(typeof(Vortex));
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    // clignement d'yeux
+    private void blink() {
+        //vignette.v
+    }
+
+
+
+
 }
