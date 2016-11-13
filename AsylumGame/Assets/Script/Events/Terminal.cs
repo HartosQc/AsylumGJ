e74501b65c855d709d37440b36c26cdf3b31827d
Maxime Hardy <maxime.hardy.1@ens.etsmtl.ca>
2016-11-13 17:04:31 -0500

    Merging Terminal Sprite and TextMesh
    Fixing terminal ouput format and merging work

--- AsylumGame/Assets/Script/Events/Terminal.cs
+++ AsylumGame/Assets/Script/Events/Terminal.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Terminal : EventTrigger {
 
-
 	public override void enterAction(GameObject player) {
 		PlayerControls playerControls = player.GetComponent<PlayerControls> ();
 		playerControls.addInteractable (this.gameObject);
