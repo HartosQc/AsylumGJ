06d48513988b2514a0f3eeca7ef11e73b38f1ca2
Julien thibault <julienthibault244@gmail.com>
2016-11-13 14:05:06 -0500

    ??

---
+++ AsylumGame/Assets/EventDispute.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventDispute : MonoBehaviour {
+
+	bool eventIsStarted = false;
+	bool eventIsOver = false;
+
+	public GameObject hearth;
+
+	slideBack slide;
+	// Use this for initialization
+	void Start () 
+	{
+		slide = GetComponent<slideBack> ();
+		EventStart();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (eventIsStarted) {
+			slide.slideUp ();
+		}
+
+		if (eventIsOver) {
+			if (slide.slideDown ()) {
+				EventDestroy ();
+			}	
+		}	
+	}
+
+	public void EventStart()
+	{
+		hearth.SetActive (true);
+		eventIsStarted = true;
+		eventIsOver = false;
+	}
+
+	public void EventOver()
+	{
+		eventIsOver = true;
+		eventIsStarted = false;
+
+	}
+
+	void EventDestroy()
+	{
+		Destroy (this.gameObject);
+	}
+}
