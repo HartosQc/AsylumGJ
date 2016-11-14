29791c238d76ae3919a441405bc9f5d7d4c462c6
Unknown <maksim.sen@hotmail.co.il>
2016-11-13 19:03:47 -0500

    camera fade in  prefab and script

---
+++ AsylumGame/Assets/maksim_scene/fadeScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fadeScript : MonoBehaviour {
+    public float fadeTime;
+    private float timePassed;
+    public float timeStartFade;
+    public float fadeSpeed;
+
+    private float alphaMat; 
+    void Start()
+    {
+        fadeSpeed /= 100; 
+    }
+    void FixedUpdate () {
+        timePassed += Time.deltaTime;
+        print(timePassed);
+        if(timePassed > timeStartFade && timePassed < timeStartFade + fadeTime)
+        {
+            alphaMat = GetComponent<Renderer>().material.color.a;
+            print(alphaMat);
+            if(alphaMat - fadeSpeed < 0)
+            {
+                alphaMat = 0;
+            }
+            else
+            {
+                alphaMat = alphaMat - fadeSpeed;
+            }
+            GetComponent<Renderer>().material.color = new Color(GetComponent<Renderer>().material.color.r, 
+                                                                GetComponent<Renderer>().material.color.g,
+                                                                GetComponent<Renderer>().material.color.b, alphaMat);
+        }
+    }
+}
